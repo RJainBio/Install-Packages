33fd445c5f60ac11f8881740aaa7ad8c32b4e595
RJainBio <raghav.jain@pnnl.gov>
2024-07-10 11:38:09 -0700

    Update test.R

--- R/test.R
+++ R/test.R
@@ -1,6 +1,17 @@
 print("Hi")
 
-# This function can be used to open all sheets from a list of excel files into separate data frames
+#' save_all_sheets
+#' 
+#' This function can be used to open all sheets from a list of excel files into separate data frames
+#'
+#' @return a set of data frames
+#' @export
+#'
+#' @examples
+#' folder_path <- "myfolder/"
+#' excel_files <- list.files(path = folder_path, pattern = "\\.xlsx$", full.names = TRUE)
+#' walk(excel_files, save_all_sheets)
+
 save_all_sheets <- function(file) {
   sheets <- excel_sheets(file)
   file_name <- sub("\\.xlsx$", "", basename(file))  # Removing extension for naming
