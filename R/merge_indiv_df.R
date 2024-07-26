a9256752511473cd2b3b6d49cef63112877a647b
RJainBio <raghav.jain@pnnl.gov>
2024-07-26 11:31:51 -0700

    Currently broken

--- R/merge_indiv_df.R
+++ R/merge_indiv_df.R
@@ -1,32 +1,30 @@
-#' merge_and_create_individual_dfs
-#'
-#' This function uses a primary dataframe and merges with secondary dataframes.
-#' The output is the secondary dataframes with the primary df columns.
-#' Unmatched rows are dropped.
-#' @return many data frames
-#'
-#' @examples
-#' df_names <- ls() # list of secondary data frames in environment
-#' primary_df <- primary_df # dataframe with info to be distributed across all others
-#' column_name <- "ID" # column common in primary and all secondary for merge
-#' merge_and_create_individual_dfs(primary_df, df_names, column_name)
+#' Merge one data frame with many
+#' @description
+#' This function uses a primary data frame and merges with secondary data frames.
+#' @return Multiple merged data frames.
+#' @param primary_df data frame with info to merge with multiple other data frames.
+#' @param df_names list of names of data frames to be merged from environment.
+#' @param column_name column to use for merging.
 #' @export
-merge_and_create_individual_dfs <- function(primary_df, df_name, column_name) {
+#' @examples
+#' # df_names <- ls() for environment
+#' # primary_df <- primary_df
+#' # column_name <- "ID"
+#' # merge_indiv_df(primary_df, df_names, column_name)
+#' # df_list.test = mget(df_names)
+#' # write_xlsx(df_list.test , "output.xlsx")
+merge_indiv_df <- function(primary_df,
+                           df_names,
+                           column_name) {
   for (df_name in df_names) {
-    # Get the secondary dataframe by name
     secondary_df <- get(df_name)
-
-    # Merge the primary dataframe with the secondary dataframe
     merged_df <- merge(primary_df, secondary_df, by = column_name)
-
-    # Create new dataframe name by appending '_merged' to the original name
-    new_df_name <- paste0(df_name) # This is unused but can be changed to rename dfs if overwrite is not desired
-
-    # Assign the merged dataframe to the new name in the global environment
+    new_df_name <- paste0(df_name)
     assign(new_df_name, merged_df, envir = .GlobalEnv)
   }
 }
 #' @export
-#' @rdname  merge_and_create_individual_dfs
+#' @rdname merge_indiv_df
+
 
 
